fc003881d1bb35b57da40405dec13b8b3891c2b6
wwecoder15 <rahmonovdavronbek770@gmail.com>
2024-11-28 14:24:50 +0500

    uptades

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+const sidebar = document.querySelector(".sidebar")
+
+function enter() {
+    sidebar.style.transform = "translate(0)"
+}
+
+function exit() {
+    sidebar.style.transform = "translate(-600px) "
+}
